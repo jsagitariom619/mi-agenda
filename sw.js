1af14ca3a4350c2ca53181d9a0789c157a24da03
jsagitariom619 <jsagitariom619@gmail.com>
2026-08-17 13:36:21 -0400

    Actualizar caché PWA para servir la versión actual

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='mi-agenda-v2';
-const ASSETS=['./','./index.html','./app.css','./app.js','./manifest.webmanifest'];
+const CACHE='mi-agenda-v4';
+const ASSETS=['./','./index.html','./modern.css','./modern.js','./mobile.css','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
