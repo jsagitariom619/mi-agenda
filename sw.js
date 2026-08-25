3ca59f1054a06149e8fbb0853ce26ef84501d1b0
jsagitariom619 <jsagitariom619@gmail.com>
2026-08-24 23:25:01 -0400

    chore: incluir moneda de viajes en cache PWA

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='mi-agenda-v10';
-const APP_SHELL=['./','./index.html','./modern.css?v=6','./mobile-drawer-fix.css?v=1','./planner-module.css?v=1','./upgrade-module.css?v=1','./modern.js?v=9','./planner-module.js?v=1','./upgrade-module.js?v=1','./loans-module.js?v=3','./loans-module.css?v=3','./manifest.webmanifest','./icon-mi-agenda.svg'];
+const CACHE='mi-agenda-v11';
+const APP_SHELL=['./','./index.html','./modern.css?v=6','./mobile-drawer-fix.css?v=1','./planner-module.css?v=1','./upgrade-module.css?v=1','./modern.js?v=9','./planner-module.js?v=1','./upgrade-module.js?v=1','./trip-currency.js?v=1','./loans-module.js?v=3','./loans-module.css?v=3','./manifest.webmanifest','./icon-mi-agenda.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
